2847a347479227120ba9267667823aa00225207b
KAUÃ HENZO DOMINGOS SAVANI <kaua.savani2@senaisp.edu.br>
2026-03-12 09:53:59 -0300

    salvando alterações

---
+++ script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", function(){
+
+    const texto = document.getElementById("texto");
+
+    texto.textContent = "Texto alterado com JavaScript!";
+
+});

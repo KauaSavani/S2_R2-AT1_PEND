7a44e86159978bdae5461b43b89d362ea191cf7a
KAUÃ HENZO DOMINGOS SAVANI <kaua.savani2@senaisp.edu.br>
2026-03-19 08:42:39 -0300

    termino dos comentarios exercicio2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const botao = document.getElementById("botao");
+const botao = document.getElementById("botao"); // Seleciona o botão pelo id
 
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ // Executa ao clicar no botão
 
-    const texto = document.getElementById("texto");
+    const texto = document.getElementById("texto"); // Seleciona o parágrafo
 
-    texto.textContent = "Texto alterado com JavaScript!";
+    texto.textContent = "Texto alterado com JavaScript!"; // Altera o texto exibido
 
 });

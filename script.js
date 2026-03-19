4791f64d8674a7a6a8227df8c402274e28291c9c
KAUÃ HENZO DOMINGOS SAVANI <kaua.savani2@senaisp.edu.br>
2026-03-19 08:43:35 -0300

    termino dos comentarios exercicio3

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const botao = document.getElementById("botaoAdicionar");
+const botao = document.getElementById("botaoAdicionar"); // Seleciona o botão adicionar
 
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ // Executa ao clicar no botão
 
-    const input = document.getElementById("item");
+    const input = document.getElementById("item"); // Seleciona o campo de texto
 
-    const texto = input.value;
+    const texto = input.value; // Pega o valor digitado
 
-    const li = document.createElement("li");
+    const li = document.createElement("li"); // Cria um item de lista
 
-    li.textContent = texto;
+    li.textContent = texto; // Define o texto do item
 
-    const lista = document.getElementById("lista");
+    const lista = document.getElementById("lista"); // Seleciona a lista (ul)
 
-    lista.appendChild(li);
+    lista.appendChild(li); // Adiciona o item na lista
 
-    input.value = "";
+    input.value = ""; // Limpa o campo de texto
 
 });

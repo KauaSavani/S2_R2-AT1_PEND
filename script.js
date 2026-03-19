cda55f7d9103e0547059e932d0a1fcf876d40ba5
KAUÃ HENZO DOMINGOS SAVANI <kaua.savani2@senaisp.edu.br>
2026-03-19 08:41:44 -0300

    termino dos comentarios exercicio1

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const botao = document.getElementById("botao");
+const botao = document.getElementById("botao"); // Seleciona o botão pelo id
 
-botao.addEventListener("click", function(){
+botao.addEventListener("click", function(){ // Executa ao clicar no botão
 
-    const nome = document.getElementById("nome").value;
+    const nome = document.getElementById("nome").value; // Pega o valor digitado no input
 
-    const mensagem = document.getElementById("mensagem");
+    const mensagem = document.getElementById("mensagem"); // Seleciona o elemento da mensagem
 
-    mensagem.textContent = "Olá " + nome + " Seja bem-vindo!";
+    mensagem.textContent = "Olá " + nome + " Seja bem-vindo!"; // Exibe a mensagem na tela
 
 });
